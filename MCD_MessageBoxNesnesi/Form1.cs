97f3ae60ceeac38e2fb6e38cd06cbf172a3d5b1d
emre gemici <gmcc.emree@gmail.com>
2021-12-08 19:09:45 +0300

    Add project files.

---
+++ MCD_MessageBoxNesnesi/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_MessageBoxNesnesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesajGoster_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kullanıcıya göstermek istediğiniz bilgi","Başlık Bilgisi",MessageBoxButtons.OK ,MessageBoxIcon.Information);
+        }
+
+        private void btnSoruSor_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Yeniden denemek ister misiniz?", "Soru", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if (res==DialogResult.Yes )
+            {
+
+            }
+
+            if (res == DialogResult.No )
+            {
+
+            }
+
+            if (res == DialogResult.Cancel )
+            {
+
+            }
+
+        }
+    }
+}
